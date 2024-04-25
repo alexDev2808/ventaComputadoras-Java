7bfe1326485d09309a2cecd00923504da2648a13
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-25 14:59:18 -0600

    Clase Monitor y realizando pruebas

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,9 +1,19 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
     }
 }
